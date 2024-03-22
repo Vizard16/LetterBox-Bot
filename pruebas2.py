38d30fedce868672db3d7d5c5e5d3fca4c3406c3
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-03-22 10:41:59 -0600

    Run command for join the bot into server

--- pruebas2.py
+++ pruebas2.py
@@ -9,3 +9,31 @@ from letterboxdpy import movie
 
 
 logger = settings.logging.getLogger("bot")
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix="!", intents= intents)
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    @bot.command()
+    async def say(ctx, *what):
+        if not what:
+        # If no arguments provided, default to "WHAT?"
+            what = ["WHAT?"]
+        await ctx.send(" ".join(what))
+
+    @bot.command()
+    async def movie_info(ctx, *, movie_title):
+        """Fetches information about a movie"""
+        movie_data = get_movie_info(movie_title)
+        embed = create_movie_embed(movie_data)
+        await ctx.send(embed=embed)
+
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
+if __name__ == "__main__":
+    run()
